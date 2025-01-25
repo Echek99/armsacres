d70fb535b423702356d3c3905d61736d0d7720a3
Echek99 <echeverrijulian100@gmail.com>
2025-01-25 00:55:55 -0500

    added 301 redirects

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,42 @@ const nextConfig: NextConfig = {
     ],
   },
   output: "standalone", // Ensure compatibility with Netlify
+
+  async redirects() {
+    return [
+      {
+        source: "/armsacres-edibles-thc-infused-edibles-nyc-delivery",
+        destination: "/edibles",
+        permanent: true, // 301 Redirect
+      },
+      {
+        source: "/thc-pens-weed-delivery-nyc-free-delivery-24-7",
+        destination: "/vapes",
+        permanent: true, // 301 Redirect
+      },
+      {
+        source: "/pre-rolls-weed-delivery-nyc-free-delivery-24-7/",
+        destination: "/pre-rolls",
+        permanent: true, // 301 Redirect
+      },
+      {
+        source: "/eights-weed-delivery-nyc-free-delivery-24-7",
+        destination: "/eighths",
+        permanent: true, // 301 Redirect
+      },
+      {
+        source: "/ounces-weed-delivery-nyc-free-delivery-24-7",
+        destination: "/ounces",
+        permanent: true, // 301 Redirect
+      },
+      {
+        source: "/blog-armsacres-cannabis-information",
+        destination: "/blog",
+        permanent: true, // 301 Redirect
+      },
+      // Add more redirects here if needed
+    ];
+  },
 };
 
 export default nextConfig;
